8136fefd9b7c8b3074d853a12c10195e6e41f66c
David Odediran <davidblack174@gmail.com>
2024-07-22 20:56:30 +0000

    update auth structure

--- website/auth.py
+++ website/auth.py
@@ -5,18 +5,22 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template("login.html")
+
 # This is the route for the sign up page
 @auth.route('/sign-up')
 def sign_up():
     return render_template("sign_up.html")
+
 # This is the route for the logout page
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
+
 # This is the route for the profile page
 @auth.route('/profile')
 def profile():
     return "<p>Profile</p>"
+
 # This is the route for the reset password page
 @auth.route('/reset-password')
 def reset_password():
