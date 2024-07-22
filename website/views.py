e861125ca5a53174b683b049111ad032e949fe62
David Odediran <davidblack174@gmail.com>
2024-07-22 22:56:23 +0000

    updates views for home page

--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
+
+# This is the route for the home page
+@views.route('/')
+def home():
+    return render_template("home.html")
