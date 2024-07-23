8f3c7b84e0d514b852de9e239ab0126c1b30c8e7
David Odediran <davidblack174@gmail.com>
2024-07-23 11:17:33 +0000

    update the following views, home.html, login.html abd sign up.html

--- website/views.py
+++ website/views.py
@@ -4,5 +4,14 @@ views = Blueprint('views', __name__)
 
 # This is the route for the home page
 @views.route('/')
+@views.route('/home')
 def home():
     return render_template("home.html")
+
+# This is the route for the about page
+@views.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+
+
